6ed71029f4995489589ca31c72954f4bb2ffa1de
cravilaedu <cravila@my365.bellevue.edu>
2025-03-20 22:52:24 -0500

    M2Discussion

---
+++ M2Discussion/M2Discussion.cpp
@@ -0,0 +1,34 @@
+// Name: Cristian Avila
+// Date: 03/20/2025
+// File Name: M2Discussion.cpp
+// Prompt: Creat a simple C++ program using input/output processes and at least two errors. The program should not execute and there should be at least two errors in the code.
+
+#include <iostream>;
+
+using namespace std;
+
+int main() {
+
+// The different variables being declared
+	int num1, num2, total;
+
+// Asking the user for input
+
+	cout << "Enter first number: ";
+	cin >> num1;
+
+// Asking the user for the second input
+
+	cout << "Enter second number: ";
+	cin >> num3;
+
+// Adding the numbers together
+
+	total = num1 + num2;
+	cout << "Total: " << total << endl:
+
+
+	return 0;
+}	
+
+ 
